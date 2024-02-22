1624554cdeae2db24086ba225eca09dc487d81ee
Rafaella-Eduarda <160852410+Rafaella-Eduarda@users.noreply.github.com>
2024-02-22 08:58:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const pessoa = "832.192.304-34"
 console.log(`conteúdo inicial da variavel: ${pessoa} | tipo: ${typeof9 (pessoa)}`);
 pessoa = 259;
 
-console.log{´ Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}');
+console.log{` Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`)
