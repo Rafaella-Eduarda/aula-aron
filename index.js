9e124235d523b9bded5dd2078c33c5cfc7f278bc
Rafaella-Eduarda <160852410+Rafaella-Eduarda@users.noreply.github.com>
2024-02-26 11:39:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const pessoa = "832.192.304-34"
+var pessoa = "832.192.304-34"
 
-console.log(`conteúdo inicial da variavel: ${pessoa} | tipo: ${typeof9 (pessoa)}`);
+console.log(`conteúdo inicial da variavel: ${pessoa} | tipo: ${typeof (pessoa)}`);
+
 pessoa = 259;
 
-console.log{` Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`)
+console.log(` Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`); 
+
+var num1 = 10
+var num2 = 20
+
+var total = 0 
+
+console.log(`Total é: ${total}`);
+
+total = num1 + num2;
+
+console.log(`Total da soma ${num1}+${num2}=${total}`);
+
+total = num1 - num2;
+
+console.log(`Total da subtração ${num1}-${num2}=${total}`);
+
+total = num1 * num2;
+
+console.log(`Total da divisão ${num1}*${num2}=${total}`);
+
+total = num1 / num2;
+
+console.log(`Total da multiplicação ${num1}/${num2}=${total}`);
